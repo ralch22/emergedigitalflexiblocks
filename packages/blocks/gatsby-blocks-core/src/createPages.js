7930a592a46985b5aa862101aacdc8d83d01d18c
Mo Awwal <gomandev@gmail.com>
2023-10-01 20:32:01 +0100

    feat: Yoest seo added

--- packages/blocks/gatsby-blocks-core/src/createPages.js
+++ packages/blocks/gatsby-blocks-core/src/createPages.js
@@ -2,7 +2,8 @@ const withDefaults = require('./utils/default.options')
 const createPostsPage = require('./pages/_posts')
 const createUserPage = require('./pages/_user')
 const createPostPage = require('./pages/_post')
-
+const createCasePage = require('./pages/_case')
+const createProductPage = require('./pages/_product')
 module.exports = async (helpers, pluginOptions) => {
   pluginOptions = withDefaults(pluginOptions)
 
@@ -12,14 +13,21 @@ module.exports = async (helpers, pluginOptions) => {
   await createPostsPage(helpers, pluginOptions, {
     template: require.resolve('./templates/posts')
   })
-  
-   /**
+
+  await createCasePage(helpers, pluginOptions, {
+    template: require.resolve('./templates/case')
+  })
+
+  await createProductPage()(helpers, pluginOptions, {
+    template: require.resolve('./templates/product')
+  })
+
+  /**
    * Posts (home) page
    */
-   await createUserPage(helpers, pluginOptions, {
+  await createUserPage(helpers, pluginOptions, {
     template: require.resolve('./templates/user')
   })
-  
 
   /**
    * Single post pages
---
+++ packages/blocks/gatsby-blocks-core/src/pages/_case.js
@@ -0,0 +1,38 @@
+const urljoin = require('url-join')
+const normalizeSlug = require('../utils/normalizeSlug')
+const fetch = require('node-fetch')
+
+module.exports = async (
+  { graphql, actions, reporter },
+  pluginOptions,
+  { template }
+) => {
+  const { createPage } = actions
+  const {
+    basePath,
+    paginatePostsPage,
+    homePostsPerPage,
+    pagingParam,
+    pageContextOptions
+  } = pluginOptions
+
+  // Fetch custom post data from WordPress REST API
+  const response = await fetch(
+    'https://emergedigital.ae/wp-json/wp/v2/case-studies'
+  )
+  const postData = await response.json()
+
+  // Load the SinglePost template
+  // const postTemplate = path.resolve('./src/templates/case.js');
+
+  // Create individual pages for each custom post
+  postData.forEach(post => {
+    createPage({
+      path: `/cases/${post.slug}`,
+      component: template,
+      context: {
+        post
+      }
+    })
+  })
+}
